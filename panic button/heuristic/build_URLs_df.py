920a4b8f7fc1ed7eeac671dd02ed1c3fdc350bdc
tomerfatael <tomerfatael@mail.tau.ac.il>
2022-08-14 20:18:07 +0300

    adding comments

--- panic button/heuristic/build_URLs_df.py
+++ panic button/heuristic/build_URLs_df.py
@@ -1,5 +1,4 @@
 import re
-from collections import defaultdict
 
 import leveldb as leveldb
 import pandas as pd
@@ -24,58 +23,63 @@ COMMON_ALERTS_WORDS = ['techsupport', 'alert', 'pc', 'security', 'windows', 'add
 COMMON_ALERTS_WORDS = list(set(COMMON_ALERTS_WORDS))
 
 if __name__ == "__main__":
+	# initializing databases from OpenWPM crawler
 	db = leveldb.LevelDB('../assets/levelDB')
 	conn = sqlite3.connect('../assets/crawl-data.sqlite')
 	cursor = conn.cursor()
 
-	# df = pd.DataFrame(columns=['url', 'js_content'])
+	# initializing data frame that we will work with
 	df = pd.DataFrame(columns=['url', 'js_content', 'visit_id', 'top_level_url', 'alerts'] + COMMON_ALERTS_WORDS + ['phone'])
 
-	cnt = 0
 	columns = cursor.execute("SELECT id, url, content_hash, visit_id FROM http_responses")
 	values = set(columns.fetchall())
+	# iterating through the database and filling the data frame
 	for value in values:
-		if cnt == 40000:
-			break
-		cnt += 1
-		print(cnt)
 		id = value[0]
 		url = value[1]
-		key = value[2]
-		visit_id = value[3]
+		key = value[2]  # will be used to retrieve the javascript content of the url from the leveldb database
+		visit_id = value[3]  # will be used to retrieve the top level url
 		try:
 			cursor.execute(f"DELETE FROM http_responses WHERE id = {id}")
 			conn.commit()
 			if key:
-				content = db.Get(key.encode('utf-8'))
+				content = db.Get(key.encode('utf-8'))  # fetching the js content with the key
 				if content:
 					content = content.decode('utf-8')
 					df = df.append({'url': url, 'js_content': content, 'visit_id': visit_id}, ignore_index=True)
 		except Exception:
 			continue
 
+	# continue filling the data frame
 	for _, row in df.iterrows():
+		# retrieving top level url for each url
 		visit_id = row['visit_id']
 		columns = cursor.execute(f"SELECT top_level_url FROM javascript WHERE visit_id = {visit_id}")
 		top_level_url = columns.fetchall()
 		if top_level_url:
 			top_level_url = top_level_url[0][0]
 			row['top_level_url'] = top_level_url
+
+		# filling the alert columns in the data frame by using js content
 		content = row['js_content']
 		if 'alert' in content:
-			alerts = re.findall(r'(?<=alert\(\").+(?=\")', content)
+			alerts = re.findall(r'(?<=alert\(\").+(?=\")', content)  # fetching the alerts functions from js content with regex
 			if alerts:
 				row['alerts'] = alerts
+				# looking for instances common alert words given by the article in the founded alerts
 				for alert in alerts:
 					for risk_word in COMMON_ALERTS_WORDS:
+						# marking the founded common alert words in the data frame
 						if risk_word in alert.lower():
 							row[risk_word] = int(1)
 							continue
+
+					# searching for US phone number patterns in the alert functions using regex
 					phone_numbers = re.findall(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})', alert)
 					if phone_numbers:
 						row['phone'] = int(1)
 
-	df = df.dropna(subset=COMMON_ALERTS_WORDS, how='all')
+	df = df.dropna(subset=COMMON_ALERTS_WORDS, how='all')  # deleting all the rows without alerts at all
 	df.drop('js_content', axis=1, inplace=True)
 	df.drop('alerts', axis=1, inplace=True)
 	df.drop(['visit_id'], axis=1, inplace=True)
--- panic button/heuristic/heuristic.py
+++ panic button/heuristic/heuristic.py
@@ -19,34 +19,33 @@ COMMON_ALERTS_WORDS = ['techsupport', 'alert', 'pc', 'security', 'windows', 'add
 
 
 if __name__ == "__main__":
+	# reading the csv file created in the build_URLs_df
 	df = pd.read_csv('data_base_actual.csv')
 
 	num_of_url = df.shape[0]
 
+	# giving each alert it's value
 	alert_weight_mapping = defaultdict(int)
 	for alert_column in df[COMMON_ALERTS_WORDS]:
 		alert_weight_mapping[alert_column] = 0
 		if num_of_url != 0:
 			alert_weight_mapping[alert_column] += (df.loc[:, alert_column].sum() / num_of_url)
 
-	# for alert in ['call', 'techsupport', 'tech support', 'contact', 'phone', 'toll-free', 'toll free']:
-	# 	alert_weight_mapping[alert]
-
+	# calculating the maximal weight possible from the data frame
 	max_weight = 0
 	for alert in COMMON_ALERTS_WORDS:
 		max_weight += alert_weight_mapping[alert]
 
+	# calculating each url it's tech support scam score based on the previous alert weights
 	df['tech_support_scam_score'] = 0
 	for row_number in range(df.shape[0]):
 		for alert_column in COMMON_ALERTS_WORDS:
-			alert_value = df.loc[row_number].at[alert_column]
+			alert_value = df.loc[row_number].at[alert_column]  # checking whether the alert has appeared in the current url
 			if pd.isnull(alert_value):
 				continue
 			new_value = df.at[row_number, 'tech_support_scam_score'] + (alert_value/max_weight * alert_weight_mapping[alert_column])
 			df.loc[row_number, ['tech_support_scam_score']] = new_value
 
+	avg_weight = df['tech_support_scam_score'].mean()  # used to define the initial tech support scam score threshold
+
 	df.to_csv('weights_actual.csv')
-
-
-	avg_weight = df['tech_support_scam_score'].mean()
-	x = 1
